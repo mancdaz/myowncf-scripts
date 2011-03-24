1567c7ad09a6afc415e2952e17bf931fc6a35894
Darren Birkett <darren.birkett@gmail.com>
2011-03-24 18:00:37 +0000

    remove api key. Again

--- resize_cserver.py
+++ resize_cserver.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
 
 import cloudservers
+import ConfigParser
 
-USERNAME = "darrenbirkett"
-AUTH_URL = "https://lon.auth.api.rackspacecloud.com/v1.0"
-API_KEY = "35fcdf2c7ccedff0e257a203d9aaabe1"
+# get our config from the config file
+config = ConfigParser.ConfigParser()
+config.read("./cf.ini")
+
+USERNAME = config.get("auth", "username")
+AUTH_URL = config.get("auth", "url")
+API_KEY = config.get("auth", "key")
 
 # Server ID to resize
 SERVER_ID = 10010692
