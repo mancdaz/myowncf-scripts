c94374538012e1c0c623a9d299ca25ea4d1a6acb
mancdaz <mancdaz1203@gmail.com>
2011-04-09 00:02:12 +0100

    got it working to resize a server, then confirm the resize

--- resize_cserver.py
+++ resize_cserver.py
@@ -11,8 +11,31 @@ USERNAME = config.get("auth", "username")
 AUTH_URL = config.get("auth", "url")
 API_KEY = config.get("auth", "key")
 # Server ID to resize
-#SERVER_ID = 10010692
+SERVER = '10003688' 
+FLAVOR = '2'
 
+# create connection object
 cnx = cloudservers.CloudServers(USERNAME, API_KEY, auth_url=AUTH_URL)
-cnx.servers.list()
-cnx.flavors.list()
+
+# get a list of servers on the account
+servers = cnx.servers.list()
+for server in servers:
+    print server 
+
+# get a list of available flavors
+flavors = cnx.flavors.list()
+for flavor in flavors:
+    print flavor
+
+# get a list of server IDs
+for server in servers:
+    print server.id
+
+# get a list of flavor IDs
+for flavor in flavors:
+    print flavor.id
+
+
+# resize specified server
+cnx.servers.resize(SERVER, FLAVOR)
+cnx.servers.confirm_resize(SERVER)
